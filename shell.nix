4d65cc9653e792b0df8e824b0c8233f99692da6c
Sergey Vlasov <sigprof@gmail.com>
2022-05-12 11:55:25 +0300

    Sync shell.nix with current QMK master (#43)
    Apply changes made in the QMK `master` branch:
    
      * qmk/qmk_firmware#14231
      * qmk/qmk_firmware#16602

--- shell.nix
+++ shell.nix
@@ -1,27 +1,13 @@
-{ avr ? true, arm ? true, teensy ? true }:
 let
   # We specify sources via Niv: use "niv update nixpkgs" to update nixpkgs, for example.
   sources = import ./nix/sources.nix { };
-  pkgs = import sources.nixpkgs { };
-
-  poetry2nix = pkgs.callPackage (import sources.poetry2nix) { };
-
-  # Builds the python env based on nix/pyproject.toml and
-  # nix/poetry.lock Use the "poetry update --lock", "poetry add
-  # --lock" etc. in the nix folder to adjust the contents of those
-  # files if the requirements*.txt files change
-  pythonEnv = poetry2nix.mkPoetryEnv {
-    projectDir = ./nix;
-    overrides = poetry2nix.overrides.withDefaults (self: super: {
-      qmk = super.qmk.overridePythonAttrs(old: {
-        # Allow QMK CLI to run "bin/qmk" as a subprocess (the wrapper changes
-        # $PATH and breaks these invocations).
-        dontWrapPythonPrograms = true;
-      });
-    });
-  };
 in
-
+# However, if you want to override Niv's inputs, this will let you do that.
+{ pkgs ? import sources.nixpkgs { }
+, poetry2nix ? pkgs.callPackage (import sources.poetry2nix) { }
+, avr ? true
+, arm ? true
+, teensy ? true }:
 with pkgs;
 let
   avrlibc = pkgsCross.avr.libcCross;
@@ -35,6 +21,21 @@ let
     "-B${avrlibc}/avr/lib/avr51"
     "-L${avrlibc}/avr/lib/avr51"
   ];
+
+  # Builds the python env based on nix/pyproject.toml and
+  # nix/poetry.lock Use the "poetry update --lock", "poetry add
+  # --lock" etc. in the nix folder to adjust the contents of those
+  # files if the requirements*.txt files change
+  pythonEnv = poetry2nix.mkPoetryEnv {
+    projectDir = ./nix;
+    overrides = poetry2nix.overrides.withDefaults (self: super: {
+      qmk = super.qmk.overridePythonAttrs(old: {
+        # Allow QMK CLI to run "qmk" as a subprocess (the wrapper changes
+        # $PATH and breaks these invocations).
+        dontWrapPythonPrograms = true;
+      });
+    });
+  };
 in
 mkShell {
   name = "qmk-firmware";
